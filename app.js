88a2e0d43ce3d5cae537176b7c40628cfe1f646c
Chloe Blazey <chloeblazey@gmail.com>
2025-01-13 22:55:29 -0800

    imports handlears and uses a template hello world

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 import express from 'express'
+import { engine } from 'express-handlebars'
 
 const app = express()
+app.engine(
+  'hbs',
+  engine({
+    defaultLayout: 'main',
+    extname: 'hbs',
+  })
+)
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Hello World!' })
+})
 
 const PORT = Number(process.env.PORT) || 3000
 app.listen(PORT, () => {
